44e8484dd91d6657b949ba305f917f09d88a693f
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-26 16:19:36 +0900

    3-2. 큰 수의 법칙

--- src/greedy/Main.java
+++ src/greedy/Main.java
@@ -1,5 +1,7 @@
 package greedy;
 
+import java.util.*;
+
 /* 3-1. 거스름돈
 public class Main {
 
@@ -13,6 +15,37 @@ public class Main {
             n %= x;
         }
         System.out.println(cnt);
+    }
+}
+ */
+
+/* 3-2. 큰 수의 법칙
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int cnt = 0, answer = 0, lt = 0;
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int k = kb.nextInt();
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        for(int i = 0; i < m; i++) {
+            if(cnt == k) {
+                lt++;
+                answer += arr[lt];
+                cnt = 0;
+                lt = 0;
+            } else {
+                answer += arr[lt];
+                cnt++;
+            }
+        }
+        System.out.println(answer);
     }
 }
  */
