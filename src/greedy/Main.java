f801a09bf245f076e1e4974714f9a15e67aff082
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-26 16:04:47 +0900

    3-1. 거스름돈

---
+++ src/greedy/Main.java
@@ -0,0 +1,18 @@
+package greedy;
+
+/* 3-1. 거스름돈
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 1260;
+        int cnt = 0;
+        int[] money = {500, 100, 50, 10};
+
+        for(int x : money) {
+            cnt += n / x;
+            n %= x;
+        }
+        System.out.println(cnt);
+    }
+}
+ */
