0f6d7f1ebcb9d043dea8e0ddaa6560e1bc010a7f
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-05 18:33:39 +0900

    6-11. 성적이 낮은 순서로 학생 출력하기

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -21,3 +21,52 @@ public class Main {
     }
 }
  */
+
+/* 6-11. 성적이 낮은 순서로 학생 출력하기
+public class Main {
+
+    static class Student implements Comparable<Student> {
+
+        private String name;
+        private int score;
+
+        public Student(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getScore() {
+            return this.score;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            if(this.score < o.score) return -1;
+            else return +1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        List<Student> studentList = new ArrayList<>();
+        int n = kb.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            String name = kb.next();
+            int score = kb.nextInt();
+            studentList.add(new Student(name, score));
+        }
+
+        Collections.sort(studentList);
+
+        for(Student x : studentList) {
+            System.out.print(x.getName() + " ");
+        }
+
+    }
+}
+ */
