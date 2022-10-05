157c4ec57779ca6596def496a8d02937c0086931
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-05 18:33:13 +0900

    6-10. 위에서 아래로

---
+++ src/sorting/Main.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.*;
+
+/* 6-10. 위에서 아래로
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
+ */
