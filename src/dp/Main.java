b2616c080d2cd5131350cc2382c6cffba568615f
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-12 16:50:46 +0900

    8-4. 피보나치 수열(바텀 업 - 반복문)

--- src/dp/Main.java
+++ src/dp/Main.java
@@ -23,3 +23,21 @@ public class Main {
     }
 }
  */
+
+/* 8-4. 피보나치 수열(바텀 업 - 반복문)
+public class Main {
+
+    public static void main(String[] args) {
+        long[] dp = new long[100];
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i <= 50; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        System.out.println(dp[50]);
+    }
+}
+ */
